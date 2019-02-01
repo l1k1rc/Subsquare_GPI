1649e02cb470d7be56ba3a0623143adc315dc940
rbarrass <raphael.barrasset@gmail.com>
2019-02-01 16:51:03 +0100

    CityFactory

---
+++ src/city/CityFactory.java
@@ -0,0 +1,32 @@
+package city;
+
+import used.Point;
+
+/**
+ *  A features factory of city object
+ * @author Raphaël
+ *
+ */
+public class CityFactory {
+	
+	/**
+	 * create a Point
+	 * @param abscisse
+	 * @param ordonne
+	 * @return Point
+	 */
+	public static Point createPoint(int abscisse, int ordonne){
+//		logger.info("Point creation at : ("+this.abscisse+","+this.ordonne+")");
+		 return new Point(abscisse, ordonne);
+	}
+
+	/**
+	 * create a citizen
+	 * @param groundType
+	 * @return Citizen
+	 */
+	public static Citizen createCitizen(String workDistrict, String originDistrict, Point position) {
+//		logger.info("Citizen creation at : ("+workDistrict+","+originDistrict+","+position.getAbscisse()+","+position.getOrdonne()+")");
+		return new Citizen(workDistrict, originDistrict, position);
+	}
+}
