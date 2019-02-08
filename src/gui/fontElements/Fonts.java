acaa830abb2021fd8f0fd922f26d0551f6081830
julom <julom@10.77.38.108>
2019-02-08 14:49:58 +0100

    initialisation de la prosperity bar

--- src/gui/fontElements/Fonts.java
+++ src/gui/fontElements/Fonts.java
@@ -13,6 +13,7 @@ public class Fonts{
 	private static Font f1=new Font("Arial rounded MT Bold", Font.PLAIN, 14);
 	private static Font f2 = new Font("C", Font.PLAIN, 15);
     private static Font fMenu = new Font("Times New Roman", Font.PLAIN, 25);
+    private static Font f3 = new Font("Times New Roman", Font.BOLD, 15);
 
     private static Color pop=new Color( 228, 89, 163); //to use later
     
@@ -27,6 +28,10 @@ public class Fonts{
 	}
 	public static Font getF2() {
 		return f2;
+	}
+	
+	public static Font getF3() {
+		return f3;
 	}
 
 	public static Font getfMenu() {
--- src/gui/frame/PanelScore.java
+++ src/gui/frame/PanelScore.java
@@ -8,32 +8,56 @@ import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingConstants;
+
+import gui.fontElements.Fonts;
 
 public class PanelScore extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	
+	private JLabel prosperityLabel = new JLabel("Prosperity Bar", JLabel.CENTER);
+	
 	private JPanel timeButton= new JPanel();
 	private JPanel scorePanel = new JPanel();
+	private JPanel prosperity = new JPanel();
 	
 	private JButton stop=new JButton(new ImageIcon("pause.png"));
 	private JButton go=new JButton(new ImageIcon("play.png"));
+	
+	private JProgressBar prosperityBar = new JProgressBar();
 	
 	public PanelScore() {
 		super();
 		setLayout(new BorderLayout());
 		setBorder(BorderFactory.createEtchedBorder());
 		
+		prosperity.setLayout(new GridLayout(6, 1));
 		scorePanel.setLayout(new GridLayout(10, 1));
 		timeButton.setLayout(new FlowLayout());
+		
+		prosperityBar = new JProgressBar(SwingConstants.VERTICAL);
+		prosperityBar.setBackground(Color.red);
+		
+		prosperityLabel.setFont(Fonts.getF3());
 		
 		go.setBackground(Color.DARK_GRAY);
 		stop.setBackground(Color.DARK_GRAY);
 		timeButton.add(go);
 		timeButton.add(stop);
+		prosperity.add(prosperityBar);
+		prosperity.add(prosperityLabel);
+		scorePanel.add(prosperity);
 		
 		add(timeButton, BorderLayout.NORTH);
+		add(scorePanel, BorderLayout.CENTER);
+	}
+	
+	public JProgressBar getProsperityBar() {
+		return prosperityBar;
 	}
 
 }
