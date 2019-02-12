f6d243c8cf48e886d92f83ff491022f3d86ad61a
rbarrass <raphael.barrasset@gmail.com>
2019-02-12 23:30:59 +0100

    TestsU

---
+++ src/test/GridBuilderTestCase.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import engine.GridBuilder;
+import engine.GridParameters;
+
+/**
+ * 
+ * This is an automated test.
+ *
+ */
+public class GridBuilderTestCase{
+	
+	GridBuilder buildGrid;
+	
+	@Before
+	public void prepareGridBuilder() {
+		GridParameters parameters = GridParameters.getInstance();
+		
+		buildGrid = new GridBuilder(parameters);
+	}
+
+	@Test
+	public void testGridBuilder() {
+		assertEquals(buildGrid.getGrid().getGridParameters(),21);
+	}
+	
+	
+	
+	@Test
+	public void testGridObstacl() {
+		assertEquals(buildGrid.getGrid().getObstacle().size(),25);
+	}
+	
+}
